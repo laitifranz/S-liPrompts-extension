1b30d06085488f644cc7fca5feee223403c3b132
Francesco Laiti <francesco.laiti@studenti.unitn.it>
2023-11-22 13:22:26 +0100

    fix typo

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_json(settings_path):
 
 
 def setup_parser():
-    parser = argparse.ArgumentParser(description='Reproduce of multiple continual learning algorthms.')
+    parser = argparse.ArgumentParser(description='Reproduce of multiple continual learning algorithms.')
     parser.add_argument('--config', type=str, default='./configs/cddb_slip.json',
                         help='Json file of settings.')
     return parser
